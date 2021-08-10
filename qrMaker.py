618975afef0d4507a8a851cfe63d272d7c285c3e
Damian Brito <brito314@gmail.com>
2021-08-09 22:07:15 -0400

    Added in AttributeError exception handling for non-connected devices

--- qrMaker.py
+++ qrMaker.py
@@ -18,56 +18,57 @@ import wifi_qrcode_generator
 Id = subprocess.check_output(
     ['netsh', 'wlan', 'show', 'interfaces']).decode('utf-8').split('\n')
 
-# eliminates unnecessary output before and after dataframe created
-connectionData = [val.split(':') for val in Id[3:]]
-toDelete = []
+try:
+    # eliminates unnecessary output before and after dataframe created
+    connectionData = [val.split(':') for val in Id[3:]]
+    toDelete = []
 
-# loops through each list in connectionData to remove whitespace and newline characters
-# andy newline chars or whitespace indexes are appended to toDelete.
-for num in range(len(connectionData)):
-    if len(connectionData[num]) > 1:
-        # mac addresses are compacted back together since they are split by line 22
-        if 'BSSID' not in connectionData[num][0] and 'Physical address' not in connectionData[num][0]:
-            connectionData[num][0] = connectionData[num][0].strip()
-            connectionData[num][1] = connectionData[num][1].rstrip('\r')
+    # loops through each list in connectionData to remove whitespace and newline characters
+    # andy newline chars or whitespace indexes are appended to toDelete.
+    for num in range(len(connectionData)):
+        if len(connectionData[num]) > 1:
+            # mac addresses are compacted back together since they are split by line 22
+            if 'BSSID' not in connectionData[num][0] and 'Physical address' not in connectionData[num][0]:
+                connectionData[num][0] = connectionData[num][0].strip()
+                connectionData[num][1] = connectionData[num][1].rstrip('\r')
+            else:
+                connectionData[num][0] = connectionData[num][0].strip()
+                mac = ':'.join(connectionData[num][1:]).strip('\r').strip()
+                connectionData[num][1] = mac
+                del connectionData[num][2:]
         else:
-            connectionData[num][0] = connectionData[num][0].strip()
-            mac = ':'.join(connectionData[num][1:]).strip('\r').strip()
-            connectionData[num][1] = mac
-            del connectionData[num][2:]
-    else:
-        toDelete.append(num)
+            toDelete.append(num)
 
-# final cleaning for pulled data
-connectionData = [val for val in connectionData if len(val) > 1]
+    # final cleaning for pulled data
+    connectionData = [val for val in connectionData if len(val) > 1]
 
-# creating dataframe with clean data
-# TODO: convert or migrate from df to series
-formattedData = pd.DataFrame(connectionData).transpose()
-newHeader = formattedData.iloc[0]
-formattedData = formattedData[1:]
-formattedData.columns = newHeader
-        
-# send netsh command to pull password and append to dataframe
-password = subprocess.check_output(
-    ['netsh', 'wlan', 'show', 'profiles',
-        formattedData.Profile.item().strip(), 'key=clear']).decode('utf-8').split('\n')
-formattedData['Password'] = str([b.split(":")[1][1:-1]
-                    for b in password if "Key Content" in b])[2:-2]
+    # creating dataframe with clean data
+    formattedData = pd.DataFrame(connectionData).transpose()
+    newHeader = formattedData.iloc[0]
+    formattedData = formattedData[1:]
+    formattedData.columns = newHeader
+            
+    # send netsh command to pull password and append to dataframe
+    password = subprocess.check_output(
+        ['netsh', 'wlan', 'show', 'profiles',
+            formattedData.Profile.item().strip(), 'key=clear']).decode('utf-8').split('\n')
+    formattedData['Password'] = str([b.split(":")[1][1:-1]
+                        for b in password if "Key Content" in b])[2:-2]
 
-# Prints out WiFi details
-# TODO: output useful info from df
-# TODO: format tab spacing between items in console
-print("\n<~~~~ WiFi Details ~~~>")
-print("    SSID:   ", formattedData.SSID.item().strip())
-print("    Radio Type:    ", formattedData['Radio type'].item().strip())
-print("    Channnel:   ", formattedData.Channel.item().strip())
-print("    Signal Strength:   ", formattedData.Signal.item().strip())
+    # Prints out WiFi details
+    print("\n<~~~~ WiFi Details ~~~>")
+    print("    SSID:   ".ljust(25), formattedData.SSID.item().strip())
+    print("    Radio Type:    ".ljust(25), formattedData['Radio type'].item().strip())
+    print("    Channnel:   ".ljust(25), formattedData.Channel.item().strip())
+    print("    Signal Strength:   ".ljust(25), formattedData.Signal.item().strip())
+    print("    Security:   ".ljust(25), formattedData.Authentication.item().strip(),'\n')
 
-# generate Qr code
-qrImg = wifi_qrcode_generator.wifi_qrcode(formattedData.Profile.item().strip(), 
-                                          False, 
-                                          'WPA', 
-                                          formattedData.Password.item().strip())
-# outputs image to default picture viewer
-qrImg.show()
+    # generate Qr code
+    qrImg = wifi_qrcode_generator.wifi_qrcode(formattedData.Profile.item().strip(), 
+                                            False, 
+                                            'WPA', 
+                                            formattedData.Password.item().strip())
+    # outputs image to default picture viewer
+    qrImg.show()
+except AttributeError:
+    print("\nNo WiFi connection found.  Check your settings and try again!\n")
